73f8e1545e5e458f53f0ac74d0950bbd33576f62
hariz <hariz.hafiz@malaysiakini.com>
2023-04-27 12:25:44 +0800

    fix: Text width keeps changing

--- src/components/Demo.svelte
+++ src/components/Demo.svelte
@@ -5,15 +5,15 @@
 
 <div class="mx-6 mt-40 flex flex-col items-center justify-center md:mx-[17.1rem] md:flex-row">
   <img src={illustrationProductive} alt="illustration-productive" class="mx-8 md:mx-0" />
-  <div class="mx-2 mt-12 flex flex-col justify-center md:mx-12 md:ml-12 md:mt-0">
-    <p class="mx-0 mb-4 font-headings text-xl font-bold text-white md:mr-20 md:text-4xl">
+  <div class="mx-2 mt-12 flex flex-col justify-center md:ml-12 md:mt-0">
+    <p class="mx-0 mb-4 font-headings text-xl font-bold text-white md:w-80 md:text-4xl">
       Stay productive, wherever you are
     </p>
-    <p class="mb-4 font-body text-[0.9rem] text-white md:text-base">
+    <p class="mb-4 font-body text-[0.9rem] text-white md:w-[550px] md:text-base">
       Never let location be an issue when accessing your files. Fylo has you covered for all your
       file storage needs.
     </p>
-    <p class="mb-4 font-body text-[0.9rem] text-white md:-mr-8 md:text-base">
+    <p class="mb-4 font-body text-[0.9rem] text-white md:-mr-8 md:w-[550px] md:text-base">
       Securely share files and folders with friends, family and colleagues for live collaboration.
       No email attachments required.
     </p>
