f81f74c332aaac87fc58e58ba64c2106222a1582
hariz <hariz.hafiz@malaysiakini.com>
2023-04-27 12:31:16 +0800

    fix: Container width keeps changing

--- src/components/Testimonials.svelte
+++ src/components/Testimonials.svelte
@@ -27,13 +27,9 @@
 </script>
 
 <div
-  class="relative grid grid-flow-col grid-cols-1 grid-rows-3 gap-10 px-8 pb-80 pt-40 md:grid-flow-row md:grid-cols-3 md:grid-rows-1 md:p-40 md:pb-96 md:pt-60"
+  class="relative grid grid-flow-col grid-cols-1 grid-rows-3 gap-10 px-8 pb-80 pt-40 md:mx-auto md:w-[80%] md:grid-flow-row md:grid-cols-3 md:grid-rows-1 md:pb-96 md:pt-60"
 >
-  <img
-    src={quotesIcon}
-    alt="quotes-icon"
-    class="absolute left-6 top-32 z-0 md:left-[9.5rem] md:top-52"
-  />
+  <img src={quotesIcon} alt="quotes-icon" class="absolute left-6 top-32 z-0 md:left-6 md:top-52" />
   {#each points as point}
     <div class="z-0 flex flex-col rounded-sm bg-dark-blue-testimonials p-6 shadow-lg md:p-8">
       <p class="font-body text-[12px] text-white md:text-base">
